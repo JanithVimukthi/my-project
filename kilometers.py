45507a0b11781a6b3a89135067bb9a1aa750c34b
vimukthi <vimukthi.17@business.mrt.ac.lk>
2020-10-09 19:18:16 +0530

    Minor valiables changed in kilometers code

--- kilometers.py
+++ kilometers.py
@@ -1,8 +1,8 @@
 Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> kilometers = float(input("Enter value in kilometers: "))
+>>> Kilometers = float(input("Enter value in Kilometers: "))
 
 conv_fac = 0.621371
 
-miles = kilometers * conv_fac
-print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+miles = Kilometers * conv_fac
+print('%0.2f Kilometers is equal to %0.2f miles' %(Kilometers,miles))
